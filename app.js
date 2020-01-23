5517d5a8ad81859c336ee08e2c0f4cf1c7ecb5a6
ry467 <59553345+eidai-RY1101@users.noreply.github.com>
2020-01-24 01:49:50 +0900

    通話終了時にcooldownをfalseにする

--- app.js
+++ app.js
@@ -4,47 +4,54 @@ const bot = new Eris(
   "NjY5MTgwOTg0ODQ3MTA2MDc3.Xig2hQ.6UNmSUjkORcQ0GRjHf6Nf_7NnGg"
 );
 
+let members, json;
+
 // Botの準備が整ったらコンソールに通知
 bot.on("ready", () => {
   console.log("Ready!");
   const status = {};
   const guild = bot.guilds.get("464290674305531905");
-  let members = guild.members.filter(function(value) {
+  members = guild.members.filter(function(value) {
     return !value.bot;
   });
 
-  let json = JSON.stringify(members);
+  json = JSON.stringify(members);
   json = JSON.parse(json);
   json[0].cooldown = true;
   json[1].cooldown = true;
 
   console.log(json);
-
-  //関数化して値渡すとFalseで返したい
-  json.filter(function(item, index) {
-    if (item.id === member.id) {
-      item.cooldown = false;
-      console.log(item.cooldown);
-    }
-  });
 });
 
+// 入室
 bot.on("voiceChannelJoin", (member, newChannel) => {
   const textChannel = newChannel.guild.channels.find(
     channel => channel.type === 0
   );
   const msg = `${member.username} が通話をはじめました`;
-  bot.createMessage(textChannel.id, msg);
+
+  json.filter(function(item, index) {
+    if (item.id === member.id && item.cooldown === true) {
+      bot.createMessage(textChannel.id, msg);
+    }
+  });
 });
 
-// voiceChannelLeave というイベントは
-// ユーザが音声チャンネルから退出した時に発火します。
+// 退室
 bot.on("voiceChannelLeave", (member, oldChannel) => {
   const textChannel = oldChannel.guild.channels.find(
     channel => channel.type === 0
   );
   const msg = `${member.username} が通話をやめました`;
-  bot.createMessage(textChannel.id, msg);
+  //IDが一致するユーザーのcooldownをFalseにして、1分後Trueにする予定
+  json.filter(function(item, index) {
+    if (item.id === member.id && item.cooldown === true) {
+      bot.createMessage(textChannel.id, msg);
+      item.cooldown = false;
+      console.log(item.cooldown);
+      //console.log(json);
+    }
+  });
 });
 
 // BotをDiscordに接続します
