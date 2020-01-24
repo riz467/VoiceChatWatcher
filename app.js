c4cdc595c8878d2720566c7c6f2fd5427bfcd256
ry467 <59553345+eidai-RY1101@users.noreply.github.com>
2020-01-25 00:34:31 +0900

    成形

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ bot.on("voiceChannelJoin", (member, newChannel) => {
   const textChannel = newChannel.guild.channels.find(
     channel => channel.id === sendtextchannel
   );
-  const msg = `${member.username}が${newChannel.name}に入室しました`;
+  const msg = `${member.username} が ${newChannel.name} に入室しました`;
   //jsonからIDを検索してcooldownの値をチェックしてfalseならメッセージ送信
   json.filter(function(item, index) {
     if (item.id === member.id && item.cooldown === false) {
@@ -43,7 +43,7 @@ bot.on("voiceChannelLeave", (member, oldChannel) => {
   const textChannel = oldChannel.guild.channels.find(
     channel => channel.id === sendtextchannel
   );
-  const msg = `${member.username}が${oldChannel.name}から退室しました`;
+  const msg = `${member.username} が${oldChannel.name} から退室しました`;
   //IDが一致するユーザーのcooldownをFalseにして、n分後Trueにする
   json.filter(function(item, index) {
     if (item.id === member.id && item.cooldown === false) {
