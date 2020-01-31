13c00a52a768a5cba0839990cc6c748eefe9f0df
riz467 <39502291+riz467@users.noreply.github.com>
2020-01-31 16:34:39 +0900

    新規メンバー参加時にリスト再構築

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const Eris = require("eris");
 const bot = new Eris(process.env.TOKEN);
 
 //.envの値をbool型に変換する
-function toBoolean (leavemessage) {
-  return leavemessage.toLowerCase() === 'true';
+function toBoolean(leavemessage) {
+  return leavemessage.toLowerCase() === "true";
 }
 
-let members, json;
+let guild,members,json;
 
 //.envファイルに記載
 const sendtextchannel = process.env.SEND_TEXT_CHANNEL; //送信するテキストチャンネルID
 const serverid = process.env.SERVER_ID; //サーバーID
 const waittime = process.env.WAIT_TIME; //いたずら防止のため最後に通知してから一定時間は通知しない(ミリ秒)
 const leavemessage = toBoolean(process.env.LEAVE_MESSAGE); //退室時のメッセージ通知(trueだとオン)
+const welcomemessage = toBoolean(process.env.WELCOME_MESSAGE); //新規メンバー通知(trueだとオン)
 
 //botの準備ができた際
 bot.on("ready", () => {
   console.log("Ready!");
-  const guild = bot.guilds.get(serverid);
+  guild = bot.guilds.get(serverid);
   members = guild.members.filter(function(value) {
     return !value.bot;
   });
@@ -28,6 +29,29 @@ bot.on("ready", () => {
   for (let i = 0; i < json.length; i++) {
     json[i].cooldown = false;
   }
+});
+
+//新規メンバーを含めたリストの再取得とか
+bot.on("guildMemberAdd", (guild, member) => {
+  const textChannel = guild.channels.find(
+    channel => channel.id === sendtextchannel
+  );
+  if (welcomemessage === true) {
+    const msg = `<@!${member.id}> ようこそ！`;
+    bot.createMessage(textChannel.id, msg);
+  }
+  console.log("新規メンバーが加わったため、リストの再取得を行います");
+  guild = bot.guilds.get(serverid);
+  members = guild.members.filter(function(value) {
+    return !value.bot;
+  });
+  
+  json = JSON.stringify(members);
+  json = JSON.parse(json);
+  for (let i = 0; i < json.length; i++) {
+    json[i].cooldown = false;
+  }
+  console.log("リストの再取得が完了しました");
 });
 
 // 入室
@@ -56,7 +80,7 @@ bot.on("voiceChannelLeave", (member, oldChannel) => {
   json.filter(function(item, index) {
     if (item.id === member.id && item.cooldown === false) {
       //leavemessageがtrueなら退室時のメッセージ通知
-      if (leavemessage === true){
+      if (leavemessage === true) {
         bot.createMessage(textChannel.id, msg); //メッセージ送信
       }
 
